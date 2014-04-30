6460b8d433bff8f294386ad65da6fce07ffb29f1
paulfryer <paulfryer@macrowebservices.com>
2014-04-30 14:27:26 -0700

    updates around nuget.

---
+++ CacheSharp/IAsyncCache.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CacheSharp
+{
+    public interface IAsyncCache<T>
+    {
+        Task PutAsync(string key, T value, TimeSpan lifeSpan);
+        Task<T> GetAsync(string key);
+        Task RemoveAsync(string key);
+    }
+}
