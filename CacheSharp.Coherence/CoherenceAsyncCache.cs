5561bb7d02223122063a15cb81db1cf22bd1528f
Paul Fryer <paulfryer@macrowebservices.com>
2014-05-06 08:54:07 -0700

    starting a coherence implementation

---
+++ CacheSharp.Coherence/CoherenceAsyncCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tangosol.Net;
+using Tangosol.Util.Filter;
+
+namespace CacheSharp.Coherence
+{
+
+    // TODO: Need to test this still!
+    public class CoherenceAsyncCache : IAsyncCache<string>, IInitializable
+    {
+        private INamedCache cache;
+
+        public async Task PutAsync(string key, string value, TimeSpan lifeSpan)
+        {
+            await Task.Run(() => cache.Add(key, value));
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            object[] values = await Task.Run(() => cache.GetValues(new KeyFilter(new ArrayList {key})));
+            return values[0] as string;
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await Task.Run(() => cache.Remove(key));
+        }
+
+        public async Task InitializeAsync(Dictionary<string, string> parameters)
+        {
+            string cacheName = parameters["CacheName"];
+            cache = CacheFactory.GetCache(cacheName);
+        }
+
+        public List<string> InitializationProperties
+        {
+            get { return new List<string> {"CacheName"}; }
+        }
+
+        public string ProviderName
+        {
+            get { return "Coherence"; }
+        }
+    }
+}
